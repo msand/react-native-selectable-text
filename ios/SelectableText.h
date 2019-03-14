e01cbd59c6321a352ac7b2c376f16e67c5ad1ba9
Shirly <1015885915@qq.com>
2019-03-14 11:36:59 +0800

    fix the bug Unhandled JS Exception: Tried to register two views with the same name RCTMultilineTextInputView

---
+++ ios/SelectableText.h
@@ -0,0 +1,17 @@
+//
+//  SelectableText.h
+//  NativeMobileApp
+//
+//  Created by Shirly.Chen on 11/3/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+#import <RCTText/RCTMultilineTextInputViewManager.h>
+#import <RCTText/RCTMultilineTextInputView.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SelectableTextManager : RCTMultilineTextInputViewManager
+@end
+
+NS_ASSUME_NONNULL_END
+
